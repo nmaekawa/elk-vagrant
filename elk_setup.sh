68eeb6df8010ea5cc7e9938bc6c2ae8d80f5dba0
nmaekawa <nmaekawa@g.harvard.edu>
2017-03-03 19:01:34 -0500

    pick es 2 or 5 via env var in provisioning

--- elk_setup.sh
+++ elk_setup.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-
-if [ $# -ne 1 ]; then
-    echo "Usage $0 <localhost_name_or_ip>"
-    exit 1
-fi
+# pass through env vars
+# $LOCALHOST_IP
+# $ES_2 non-zero means elasticsearch 2, 5 otherwise
 
 # prevents trying to access stdin
 export DEBIAN_FRONTEND=noninteractive
@@ -22,22 +20,47 @@ echo "export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64" >> /etc/environment
 
 # download and install elastic public key
 wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
+wget -qO - https://packages.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
 
-# install transport-https and repository definition
+
+# install transport-https
 apt-get -y install apt-transport-https
-echo "deb https://artifacts.elastic.co/packages/5.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-5.x.list
 
-# install elasticsearch and kibana
-apt-get update && apt-get install -y elasticsearch && apt-get install -y kibana
+if [ -n "${ES_2}" ]; then  # es2
+    # save repository definition
+    echo "deb http://packages.elastic.co/elasticsearch/2.x/debian stable main" | sudo tee -a /etc/apt/sources.list.d/elasticsearch-2.x.list
+    echo "deb https://packages.elastic.co/kibana/4.6/debian stable main" | sudo tee -a /etc/apt/sources.list.d/kibana.list
+
+    # install elasticsearch and kibana
+    apt-get update && apt-get install -y elasticsearch=2.4.1 && apt-get install -y kibana=4.6.2
+
+    # config files for kibana
+    KIBANA_CONFIG_FILE="/opt/kibana/config/kibana.yml"
+
+else  # es5
+    # save repository definition
+    echo "deb https://artifacts.elastic.co/packages/5.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-5.x.list
+
+    # install elasticsearch and kibana
+    apt-get update && apt-get install -y elasticsearch && apt-get install -y kibana
+
+    # config files for kibana
+    KIBANA_CONFIG_FILE="/etc/kibana/kibana.yml"
+fi
+
+
+echo "--------------------------------------"
+echo "${KIBANA_CONFIG_FILE}"
+echo "--------------------------------------"
 
 # config elasticsearch
 echo "cluster.name: escluster" >> /etc/elasticsearch/elasticsearch.yml
 echo "node.name: esnode" >> /etc/elasticsearch/elasticsearch.yml
-echo "network.host: \"${1}\"" >> /etc/elasticsearch/elasticsearch.yml
+echo "network.host: \"${LOCALHOST_IP}\"" >> /etc/elasticsearch/elasticsearch.yml
 
 # config kibana
-echo "server.host: \"${1}\"" >> /etc/kibana/kibana.yml
-echo "elasticsearch.url: \"http://${1}:9200\""
+echo "server.host: \"${LOCALHOST_IP}\"" >> ${KIBANA_CONFIG_FILE}
+echo "elasticsearch.url: \"http://${LOCALHOST_IP}:9200\"" >> ${KIBANA_CONFIG_FILE}
 
 # reload systemd configs, enable, and start elasticsearch and kibana
 systemctl daemon-reload
